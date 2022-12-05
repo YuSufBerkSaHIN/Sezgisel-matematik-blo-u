9e80f8fa2ce38a509660ae099dd20f2d5790731e
YuSufBerkSaHIN <116167645+YuSufBerkSaHIN@users.noreply.github.com>
2022-12-05 15:43:57 +0300

    Add files via upload

---
+++ Sezgisel matematik.py
@@ -0,0 +1,12 @@
+Bir sayının sıfaıra olan uzaklığı na osayının mutlak değeri denir.
+5 veya (-5) in sıra olan uzaklığı 5 birimdir
+|-5| in sıfıra olan uzaklığı 5 birimdir 
+|5| in sıfıra olan uzaklığı da 5 birimdir 
+bu nedenle ;
+|-5|=5
+|5|=5 dir.
+ Mutlak değer denklemlerinde sonuç negatifse çözük kümememiz boş kümedir çünkü;
+ mutlak değer sıfıra olan uzaklık olduğundan mesafa negatif olamaz
+
+ Örnek 1:|3x+6|=-5 denkleminin çözüm kümesi 
+ ÇK=Boş küme'dir.
